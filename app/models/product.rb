dd46a000009593f8c98ad76394a5187de1483f8c
stephany ramirez <stephany_re@hotmail.com>
2022-05-20 23:35:13 -0700

    search bar working but it hides

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,26 @@
 class Product < ApplicationRecord
   belongs_to :category
 
+  include PgSearch::Model
+  pg_search_scope :global_search,
+        against: [:title],
+        associated_against: {
+          category: [:title]
+        },
+        using: {
+          tsearch: { prefix: true }
+        }
+
   def price_with_discount(nprice = 0)
     case self.discount
     when 1
-      new_price = self.price / 2
+      self.price / 2
     when 2
-      new_price = nprice
+      nprice
     when 3
-      new_price = self.price * 0.66666666
+      self.price * 0.66666666
     else
-      new_price = self.price
+      self.price
     end
-    new_price
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'categories#index'
 
-  resources :categories, only: [:index] do
-    resources :products, only: [:index]
-  end
+  resources :categories, only: [:index, :show]
+  resources :products, only: [:index]
 
   get '/cart', to: 'order_items#index'
   resources :order_items, path: '/cart/items'
