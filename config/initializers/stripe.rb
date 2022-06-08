d22c00c3678274c2ab08c410919449401818a504
stephany ramirez <stephany_re@hotmail.com>
2022-06-07 18:59:01 -0700

    api change

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key: ENV['STRIPE_SECRET_KEY']
+  publishable_key: 'pk_test_51KxnVhA0uIQ7R28yhLD8zd6FglUmvCOyvsaXX0boaL8vmOvc88sGyIOXOF4CdMH7cUrOLfKyBuW4cZXzixTs7uwO00gB9HsQon',
+  secret_key: 'sk_test_51KxnVhA0uIQ7R28yuAxAoGO6Y4TXDdcVvoEfEWCOmDjNztjFSGgPHgfQ0g4R3Twgzj4wrqNi1HO2aV5DpyIlJaFs00Igr3PyEW'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
